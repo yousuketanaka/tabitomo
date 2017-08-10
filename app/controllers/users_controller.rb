a8c4c787611fcdf69e83c250bcf9f023690ef9c2
yousuketanaka <yousuketanaka11@gmail.com>
2017-08-10 10:57:48 +0000

    edit profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,4 @@ class UsersController < ApplicationController
   def show
       @user = User.find_by(id: params[:id])
   end
-  
-  def country_name
-    country = ISO3166::Country[country_code]
-    country.translations[I18n.locale.to_s] || country.name
-  end
 end
