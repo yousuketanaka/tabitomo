41d787c06a4c87e49159865ac976e418d87b9e65
yousuketanaka <yousuketanaka11@gmail.com>
2017-08-15 15:03:29 +0000

    change model

---
+++ db/migrate/20170815083434_remove_column_from_users.rb
@@ -0,0 +1,7 @@
+class RemoveColumnFromUsers < ActiveRecord::Migration
+  def change
+    remove_column :users, :language_1, :string
+    remove_column :users, :resident_city, :string
+    remove_column :users, :resident_country, :stirng
+  end
+end
